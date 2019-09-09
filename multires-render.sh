4e0c98eb42015e89648613bd20ef1cd6c35aeea1
Joshua Blake <joshblake@gmail.com>
2019-09-08 23:15:33 -0700

    Fix path issue in build-multires.sh Rename build-multires.sh to multires-render.sh

--- multires-render.sh
+++ multires-render.sh
@@ -6,14 +6,16 @@ experiment='001'
 path='/data/jupyter/generative/BBMSG-GAN/sourcecode/samples'
 outpath='/data/jupyter/videos'
 
+script_dir=`dirname "$0"`
+
 declare -a res_dirs=('4_x_4' '8_x_8' '16_x_16' '32_x_32' '64_x_64' '128_x_128')
 
 for res_dir in "${res_dirs[@]}"
 do
 	dir="${path}/${experiment}/${res_dir}/"
-	/data/jupyter/util/rename-pad.py ${dir}*.png
+	$script_dir/rename-pad.py ${dir}*.png
 
 	outfile="${outpath}/${network}-${dataset}-${experiment}-${res_dir//_}.mp4"
 	echo "${dir} -> ${outfile}"
-	./make-mp4.sh ${dir} ${outfile} gen_ 000001
+	$script_dir/make-mp4.sh ${dir} ${outfile} gen_ 000001
 done
